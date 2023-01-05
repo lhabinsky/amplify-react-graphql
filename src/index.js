39170ab1763c22f703ea869f600b697dacfa920f
lhabinsky <lhabinsky@live.com>
2023-01-04 20:48:21 -0500

    test git push

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import config from './aws-exports';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
+
 import '@fontsource/roboto/700.css';
 
 Amplify.configure(config);
